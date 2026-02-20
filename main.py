f2c868d857b01a0c4e82a8199dfd01e0e8c1b8b5
José Ramón Morera <91540966+jose-r-morera@users.noreply.github.com>
2026-02-20 13:05:45 +0000

    Revert "Added feaure to run all the ucr datasets with dataset=all"

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from src.utils import load_data, znormalisation, create_directory
 import sys
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # Disable TF warnings
-
 import pandas as pd
 import numpy as np
 import json
@@ -12,7 +10,6 @@ from distutils.util import strtobool
 from src.classifiers import LITE, LITEMV
 
 from sklearn.metrics import accuracy_score
-from aeon.datasets.tsc_datasets import univariate
 
 
 def get_args():
@@ -61,140 +58,135 @@ if __name__ == "__main__":
     output_directory_parent = output_directory_parent + args.classifier + "/"
     create_directory(output_directory_parent)
 
-    if (args.dataset == "all") or (args.dataset == "ALL"):
-        print(f"Total datasets: {len(univariate)}") # Output: Total datasets: 128
-        for i, dataset in enumerate(univariate):
-            print(f"Running {args.classifier} on {dataset} dataset. {i+1}/{len(univariate)}.")
-            args.dataset = dataset
-            xtrain, ytrain, xtest, ytest = load_data(file_name=args.dataset)
-            
-            length_TS = int(xtrain.shape[1])
-            n_channels = int(xtrain.shape[2])
+    xtrain, ytrain, xtest, ytest = load_data(file_name=args.dataset)
+    
+    length_TS = int(xtrain.shape[1])
+    n_channels = int(xtrain.shape[2])
 
-            # Check if the dataset has already been run and if so, skip it.
-            if os.path.exists(output_directory_parent + "results_ucr.csv"):
-                df = pd.read_csv(output_directory_parent + "results_ucr.csv")
+    # Check if the dataset has already been run and if so, skip it.
+    if os.path.exists(output_directory_parent + "results_ucr.csv"):
+        df = pd.read_csv(output_directory_parent + "results_ucr.csv")
 
-                file_names = list(df["dataset"])
-                if args.dataset in file_names:
-                    exit()
-            else:
-                if args.track_emissions:
-                    df = pd.DataFrame(
-                        columns=[
-                            "dataset",
-                            args.classifier + "-mean",
-                            args.classifier + "-std",
-                            args.classifier + "Time",
-                            "Training Time-mean",
-                            "Testing Time-mean",
-                            "CO2 Consumption-mean",
-                            "Energy Consumption-mean",
-                            "Country",
-                            "Region",
-                        ]
-                    )
-                else:
-                    df = pd.DataFrame(
-                        columns=[
-                            "dataset",
-                            args.classifier + "-mean",
-                            args.classifier + "-std",
-                            args.classifier + "Time",
-                        ]
-                    )
+        file_names = list(df["dataset"])
+        if args.dataset in file_names:
+            exit()
+    else:
+        if args.track_emissions:
+            df = pd.DataFrame(
+                columns=[
+                    "dataset",
+                    args.classifier + "-mean",
+                    args.classifier + "-std",
+                    args.classifier + "Time",
+                    "Training Time-mean",
+                    "Testing Time-mean",
+                    "CO2 Consumption-mean",
+                    "Energy Consumption-mean",
+                    "Country",
+                    "Region",
+                ]
+            )
+        else:
+            df = pd.DataFrame(
+                columns=[
+                    "dataset",
+                    args.classifier + "-mean",
+                    args.classifier + "-std",
+                    args.classifier + "Time",
+                ]
+            )
 
-            ypred = np.zeros(shape=(len(ytest), len(np.unique(ytest))))
+    ypred = np.zeros(shape=(len(ytest), len(np.unique(ytest))))
 
-            Scores = []
+    Scores = []
 
+    if args.track_emissions:
+        training_time = []
+        testing_time = []
+
+        co2_consumption = []
+        energy_consumption = []
+
+    for _run in range(args.runs):
+        output_directory = output_directory_parent + "run_" + str(_run) + "/"
+        create_directory(output_directory)
+        output_directory = output_directory + args.dataset + "/"
+        create_directory(output_directory)
+
+        if args.classifier == "LITE":
+            clf = LITE(
+                output_directory=output_directory,
+                length_TS=length_TS,
+                n_channels=n_channels,
+                n_classes=len(np.unique(ytrain)),
+            )
+        elif args.classifier == "LITEMV":
+            clf = LITEMV(
+                output_directory=output_directory,
+                length_TS=length_TS,
+                n_channels=n_channels,
+                n_classes=len(np.unique(ytrain)),
+            )
+        else:
+            raise ValueError("Choose an existing classifier.")
+
+        if not os.path.exists(output_directory + "loss.pdf"):
             if args.track_emissions:
-                training_time = []
-                testing_time = []
-
-                co2_consumption = []
-                energy_consumption = []
-
-            for _run in range(args.runs):
-                output_directory = output_directory_parent + "run_" + str(_run) + "/"
-                create_directory(output_directory)
-                output_directory = output_directory + args.dataset + "/"
-                create_directory(output_directory)
-
-                if args.classifier == "LITE":
-                    clf = LITE(
-                        output_directory=output_directory,
-                        length_TS=length_TS,
-                        n_channels=n_channels,
-                        n_classes=len(np.unique(ytrain)),
-                    )
-                elif args.classifier == "LITEMV":
-                    clf = LITEMV(
-                        output_directory=output_directory,
-                        length_TS=length_TS,
-                        n_channels=n_channels,
-                        n_classes=len(np.unique(ytrain)),
-                    )
-                else:
-                    raise ValueError("Choose an existing classifier.")
-
-                if not os.path.exists(output_directory + "loss.pdf"):
-                    if args.track_emissions:
-                        dict_emissions = clf.fit_and_track_emissions(
-                            xtrain=xtrain, ytrain=ytrain, xval=xtest, yval=ytest, plot_test=True
-                        )
-                    else:
-                        clf.fit(
-                            xtrain=xtrain, ytrain=ytrain, xval=xtest, yval=ytest, plot_test=True
-                        )
-
-                else:
-                    if args.track_emissions:
-                        with open(output_directory + "dict_emissions.json") as json_file:
-                            dict_emissions = json.load(json_file)
-
-                if args.track_emissions:
-                    co2_consumption.append(dict_emissions["co2"])
-                    energy_consumption.append(dict_emissions["energy"])
-
-                    training_time.append(dict_emissions["duration"])
-
-                    y_pred, acc, duration_test = clf.predict(xtest=xtest, ytest=ytest)
-
-                    testing_time.append(duration_test)
-
-                else:
-                    y_pred, acc, _ = clf.predict(xtest=xtest, ytest=ytest)
-
-                ypred = ypred + y_pred
-
-                Scores.append(acc)
-
-            ypred = ypred / (args.runs * 1.0)
-            ypred = np.argmax(ypred, axis=1)
-
-            acc_Time = accuracy_score(y_true=ytest, y_pred=ypred, normalize=True)
-
-            if args.track_emissions:
-                df.loc[len(df)] = {
-                    "dataset": args.dataset,
-                    args.classifier + "-mean": np.mean(Scores),
-                    args.classifier + "-std": np.std(Scores),
-                    args.classifier + "Time": acc_Time,
-                    "Training Time-mean": np.mean(training_time),
-                    "Testing Time-mean": np.mean(testing_time),
-                    "CO2 Consumption-mean": np.mean(co2_consumption),
-                    "Energy Consumption-mean": np.mean(energy_consumption),
-                    "Country": str(dict_emissions["country_name"]),
-                    "Region": str(dict_emissions["region"]),
-                }
+                dict_emissions = clf.fit_and_track_emissions(
+                    xtrain=xtrain, ytrain=ytrain, xval=xtest, yval=ytest, plot_test=True
+                )
             else:
-                df.loc[len(df)] = {
-                    "dataset": args.dataset,
-                    args.classifier + "-mean": np.mean(Scores),
-                    args.classifier + "-std": np.std(Scores),
-                    args.classifier + "Time": acc_Time,
-                }
+                clf.fit(
+                    xtrain=xtrain, ytrain=ytrain, xval=xtest, yval=ytest, plot_test=True
+                )
 
-            # Save the mean results across all n runs in a csv file.
-            df.to_csv(output_directory_parent + "results_ucr.csv", index=False)
+        else:
+            if args.track_emissions:
+                with open(output_directory + "dict_emissions.json") as json_file:
+                    dict_emissions = json.load(json_file)
+
+        if args.track_emissions:
+            co2_consumption.append(dict_emissions["co2"])
+            energy_consumption.append(dict_emissions["energy"])
+
+            training_time.append(dict_emissions["duration"])
+
+            y_pred, acc, duration_test = clf.predict(xtest=xtest, ytest=ytest)
+
+            testing_time.append(duration_test)
+
+        else:
+            y_pred, acc, _ = clf.predict(xtest=xtest, ytest=ytest)
+
+        ypred = ypred + y_pred
+
+        Scores.append(acc)
+
+    ypred = ypred / (args.runs * 1.0)
+    ypred = np.argmax(ypred, axis=1)
+
+    acc_Time = accuracy_score(y_true=ytest, y_pred=ypred, normalize=True)
+
+    if args.track_emissions:
+        df.loc[len(df)] = {
+            "dataset": args.dataset,
+            args.classifier + "-mean": np.mean(Scores),
+            args.classifier + "-std": np.std(Scores),
+            args.classifier + "Time": acc_Time,
+            "Training Time-mean": np.mean(training_time),
+            "Testing Time-mean": np.mean(testing_time),
+            "CO2 Consumption-mean": np.mean(co2_consumption),
+            "Energy Consumption-mean": np.mean(energy_consumption),
+            "Country": str(dict_emissions["country_name"]),
+            "Region": str(dict_emissions["region"]),
+        }
+    else:
+        df.loc[len(df)] = {
+            "dataset": args.dataset,
+            args.classifier + "-mean": np.mean(Scores),
+            args.classifier + "-std": np.std(Scores),
+            args.classifier + "Time": acc_Time,
+        }
+
+    # Save the mean results across all n runs in a csv file.
+    df.to_csv(output_directory_parent + "results_ucr.csv", index=False)
