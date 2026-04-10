9a98065623da9fd4082812e1138bdb3ac1520f52
José R. <alu010471846@ull.edu.es>
2026-04-10 07:41:58 +0100

    Global min-max normalization

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -21,12 +21,28 @@ def load_data(file_name):
     ytrain, ytest = encode_labels(ytrain, ytest)
     
     # Calculate global constants on train, apply to train
-    xtrain, train_mean, train_std = global_znormalisation(xtrain)
+    xtrain, train_min, train_max = global_min_max_normalisation(xtrain)
     
     # Reuse train constants to apply to test
-    xtest, _, _ = global_znormalisation(xtest, mean=train_mean, std=train_std)
+    xtest, _, _ = global_min_max_normalisation(xtest, x_min=train_min, x_max=train_max)
 
     return xtrain, ytrain, xtest, ytest
+
+
+def global_min_max_normalisation(x, x_min=None, x_max=None):
+    
+    # If min and max are not provided, calculate them globally
+    if x_min is None or x_max is None:
+        x_min = np.min(x)
+        x_max = np.max(x)
+        
+    # Prevent division by zero if the entire dataset is a constant value
+    range_val = x_max - x_min
+    if range_val == 0.0:
+        range_val = 1.0
+        
+    x_norm = (x - x_min) / range_val
+    return x_norm, x_min, x_max
 
 
 def global_znormalisation(x, mean=None, std=None):
@@ -49,8 +65,6 @@ def znormalisation(x):
         stds[stds == 0.0] = 1.0
         return (x - x.mean(axis=1, keepdims=True)) / stds
     return (x - x.mean(axis=1, keepdims=True)) / (x.std(axis=1, keepdims=True))
-
-
 
 def encode_labels(ytrain, ytest):
 
